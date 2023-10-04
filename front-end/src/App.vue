f4bfed0910889b59adc1d70093ba5f6274995eb9
Daniel-dadu <dadu9494@gmail.com>
2023-10-04 15:50:38 -0700

    Created Vue project and added simple NavBar

---
+++ front-end/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+/* * {
+  background-color: #3e4451;
+} */
+</style>
