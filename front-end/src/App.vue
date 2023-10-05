74adadde9debead2a18a885e1231f806dd3f3162
Daniel-dadu <dadu9494@gmail.com>
2023-10-05 09:21:41 -0700

    Fixed tabs

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <HelloWorld/>
+    <HelloWorld/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		HelloWorld
+	}
 }
 </script>
 
 <style>
-/* * {
-  background-color: #3e4451;
-} */
+
 </style>
--- front-end/src/components/HelloWorld.vue
+++ front-end/src/components/HelloWorld.vue
@@ -4,42 +4,42 @@
  -->
 
 <template>
-  <div id="main-nav-div">
-    <ul class="nav nav-pills nav-fill flex-column flex-sm-row" id="pills-tab" role="tablist">
-      <li class="nav-item" role="presentation">
-        <button class="nav-link active" id="pills-expenses-tab" data-bs-toggle="pill" data-bs-target="#pills-expenses" type="button" role="tab" aria-controls="pills-expenses" aria-selected="true">Expenses</button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button class="nav-link" id="pills-savings-tab" data-bs-toggle="pill" data-bs-target="#pills-savings" type="button" role="tab" aria-controls="pills-savings" aria-selected="false">Savings</button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button class="nav-link" id="pills-budget-tab" data-bs-toggle="pill" data-bs-target="#pills-budget" type="button" role="tab" aria-controls="pills-budget" aria-selected="false">Budget</button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button class="nav-link" id="pills-summary-tab" data-bs-toggle="pill" data-bs-target="#pills-summary" type="button" role="tab" aria-controls="pills-summary" aria-selected="false">Summary</button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
-      </li>
-      
-    </ul>
-  </div>
+	<div id="main-nav-div">
+		<ul class="nav nav-pills nav-fill flex-column flex-sm-row" id="pills-tab" role="tablist">
+		<li class="nav-item" role="presentation">
+			<button class="nav-link active" id="pills-expenses-tab" data-bs-toggle="pill" data-bs-target="#pills-expenses" type="button" role="tab" aria-controls="pills-expenses" aria-selected="true">Expenses</button>
+		</li>
+		<li class="nav-item" role="presentation">
+			<button class="nav-link" id="pills-savings-tab" data-bs-toggle="pill" data-bs-target="#pills-savings" type="button" role="tab" aria-controls="pills-savings" aria-selected="false">Savings</button>
+		</li>
+		<li class="nav-item" role="presentation">
+			<button class="nav-link" id="pills-budget-tab" data-bs-toggle="pill" data-bs-target="#pills-budget" type="button" role="tab" aria-controls="pills-budget" aria-selected="false">Budget</button>
+		</li>
+		<li class="nav-item" role="presentation">
+			<button class="nav-link" id="pills-summary-tab" data-bs-toggle="pill" data-bs-target="#pills-summary" type="button" role="tab" aria-controls="pills-summary" aria-selected="false">Summary</button>
+		</li>
+		<li class="nav-item" role="presentation">
+			<button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+		</li>
+		
+		</ul>
+	</div>
     <div class="tab-content" id="pills-tabContent">
-      <div class="tab-pane fade show active" id="pills-expenses" role="tabpanel" aria-labelledby="pills-expenses-tab" tabindex="0">
-        <ExpensesPage/>
-      </div>
-      <div class="tab-pane fade" id="pills-savings" role="tabpanel" aria-labelledby="pills-savings-tab" tabindex="0">
-        SAVINGS COMPONENT
-      </div>
-      <div class="tab-pane fade" id="pills-summary" role="tabpanel" aria-labelledby="pills-summary-tab" tabindex="0">
-        SUMMARY COMPONENT
-      </div>
-      <div class="tab-pane fade" id="pills-budget" role="tabpanel" aria-labelledby="pills-budget-tab" tabindex="0">
-        BUDGET COMPONENT
-      </div>
-      <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">
-        PROFILE COMPONENT
-      </div>
+		<div class="tab-pane fade show active" id="pills-expenses" role="tabpanel" aria-labelledby="pills-expenses-tab" tabindex="0">
+			<ExpensesPage/>
+		</div>
+		<div class="tab-pane fade" id="pills-savings" role="tabpanel" aria-labelledby="pills-savings-tab" tabindex="0">
+			SAVINGS COMPONENT
+		</div>
+		<div class="tab-pane fade" id="pills-summary" role="tabpanel" aria-labelledby="pills-summary-tab" tabindex="0">
+			SUMMARY COMPONENT
+		</div>
+		<div class="tab-pane fade" id="pills-budget" role="tabpanel" aria-labelledby="pills-budget-tab" tabindex="0">
+			BUDGET COMPONENT
+		</div>
+		<div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">
+			PROFILE COMPONENT
+		</div>
     </div>
 </template>
 
@@ -55,7 +55,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #main-nav-div {
-  /* background-color: aqua; */
-  padding: 2em;
+	/* background-color: aqua; */
+	padding: 2em;
 }
 </style>
