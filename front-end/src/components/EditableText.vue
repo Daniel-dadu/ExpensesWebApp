a564434c77cc4dbebb07fde4a8f7fa337a17b482
Daniel-dadu <dadu9494@gmail.com>
2023-10-06 12:10:19 -0600

    Updated EditableText component to use the Composition API #2

--- front-end/src/components/EditableText.vue
+++ front-end/src/components/EditableText.vue
@@ -1,32 +1,51 @@
 <template>
     <div v-if="!isEditing" @click="startEditing">{{ text }}</div>
     <input v-else v-model="editedText" @keyup.enter="finishEditing" ref="textInput" />
-</template>
+  </template>
   
-<script>
-export default {
-    data() {
-        return {
-            isEditing: false,
-            editedText: "",
-        };
-    },
+  <script>
+  import { ref, watch, nextTick } from "vue";
+  
+  export default {
     props: {
-        text: String, // The initial text
+      text: String, // The initial text
     },
-    methods: {
-        startEditing() {
-            this.isEditing = true;
-            this.editedText = this.text;
-            // Focus the input field after rendering
-            this.$nextTick(() => { this.$refs.textInput.focus(); });
-        },
-        finishEditing() {
-            this.isEditing = false;
-            // Update the text with the edited value
-            this.$emit("update:text", this.editedText);
-        },
+    setup(props, { emit }) {
+      const isEditing = ref(false);
+      const editedText = ref(props.text);
+      const textInputRef = ref(null);
+  
+      const startEditing = () => {
+        isEditing.value = true;
+        editedText.value = props.text;
+        // Use watch to wait for textInputRef to become available
+        watch(
+          () => textInputRef.value,
+          (newValue) => {
+            if (newValue) {
+              // Focus the input field
+              nextTick(() => {
+                newValue.focus();
+              });
+            }
+          }
+        );
+      };
+  
+      const finishEditing = () => {
+        isEditing.value = false;
+        // Use `emit` from `defineEmits` to emit the event
+        emit("update:text", editedText.value);
+      };
+  
+      return {
+        isEditing,
+        editedText,
+        textInputRef,
+        startEditing,
+        finishEditing,
+      };
     },
-};
-</script>
+  };
+  </script>
   
