0f9ef3d9440632a2f94469eb4c5e642280b8eae1
Daniel-dadu <dadu9494@gmail.com>
2023-10-20 12:41:01 -0600

    Close #27 after finished adding syles

--- front-end/src/pages/ProfilePage.vue
+++ front-end/src/pages/ProfilePage.vue
@@ -1,9 +1,23 @@
 <template>
-    <h2>Profile</h2>
-    <img :src="userImg" alt="Profile picture">
-    <h3>{{ userName }}</h3>
-    <h4>{{ userEmail }}</h4>
-    <button @click="onLogout">logout</button>
+    <img :src="userImg" class="rounded mx-auto d-block profile-margin-top" alt="Profile picture">
+    <h3 class="display-6 text-center profile-margin-top">{{ userName }}</h3>
+    <h4 class="lead text-center" style="font-style: italic;">{{ userEmail }}</h4>
+    <button 
+        type="button" 
+        class="btn btn-outline-light btn-center" 
+        @click="onLogout"
+        style="margin-top: 2rem;"
+    >
+        Log out
+    </button>
+    <button 
+        type="button" 
+        class="btn btn-outline-danger btn-center" 
+        @click="onDeleteAccount"
+        style="margin-top: 1rem;"
+    >
+        Delete account and data
+    </button>
 </template>
 
 <script setup>
@@ -24,4 +38,18 @@ const onLogout = () => {
     router.push("/login")
 }
 
+const onDeleteAccount = () => {
+    // TODO: Delete Data
+    onLogout()
+}
 </script>
+
+<style>
+.profile-margin-top {
+    margin-top: 2rem;
+}
+.btn-center {
+    display: block;
+    margin: auto;
+}
+</style>
