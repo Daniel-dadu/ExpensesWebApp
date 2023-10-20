8ae95982b7583e7e8a2b8ce9d4bcf9a025c8f0d5
Daniel-dadu <dadu9494@gmail.com>
2023-10-20 13:17:43 -0600

    Imporved styles of login page #28

--- front-end/src/pages/LoginPage.vue
+++ front-end/src/pages/LoginPage.vue
@@ -1,16 +1,26 @@
 <template>
-    <h1>Login</h1>
-    <button @click="onLogin">Login with Google</button>
+    <h3 class="text-center" style="margin-top: 2rem;">Personal Finances App</h3>
+    <button @click="onLogin" class="btn btn-outline-light google-login-btn">
+        <img src="../assets/google.svg" alt="">
+        <span>Login with Google</span>
+    </button>
 </template>
 
 <script setup>
+import { onMounted } from "vue"
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth"
 import { useRouter } from "vue-router"
 
+const router = useRouter()
+
+onMounted(() => {
+    if(window.localStorage.length !== 0) {
+        router.push("/")
+    }
+})
+
 const googleProvider = new GoogleAuthProvider()
 const auth = getAuth()
-
-const router = useRouter()
 
 const onLogin = () => {
     signInWithPopup(auth, googleProvider)
@@ -27,3 +37,26 @@ const onLogin = () => {
     })
 }
 </script>
+
+<style>
+.google-login-btn {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.google-login-btn span {
+    font-size: 1.5rem;
+}
+.google-login-btn img {
+    filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(101deg) brightness(103%) contrast(101%);
+    margin-bottom: 0.3rem;
+    margin-right: 0.7rem;
+    width: 2rem;
+}
+
+.google-login-btn:hover img {
+    filter: invert(12%) sepia(21%) saturate(342%) hue-rotate(168deg) brightness(91%) contrast(92%);
+}
+</style>
